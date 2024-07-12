900a8ba668a4c9d14ebbfedd532933fb2129b54d
Daniel160407 <abulashvilidani@hotmail.com>
2024-07-13 00:55:39 +0400

    Added ability to add external background images

--- src/components/control/BackgroundController.jsx
+++ src/components/control/BackgroundController.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../style/BackgroundController.scss';
 
 const BackgroundController = ({ setBackground }) => {
+    const [specificBackground, setSpecificBackground] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
     const elements = [];
     for (let i = 1; i <= 20; i++) {
         elements.push(
@@ -12,9 +15,24 @@ const BackgroundController = ({ setBackground }) => {
         );
     }
 
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const fileURL = URL.createObjectURL(file);
+            setSpecificBackground(fileURL);
+            setBackground(fileURL);
+        }
+    };
+
     return (
         <div className="background-controller">
             {elements}
+            <div className='specific-background'>
+                <input type='radio' name='background' value={imageUrl} onChange={(e) => setBackground(e.target.value)}></input>
+                <input type='text' value={imageUrl} onChange={(e) => setImageUrl(e.target.value)}></input>
+                <input type='radio' name='background' value={specificBackground} onChange={(e) => setBackground(e.target.value)}></input>
+                <input type='file' onChange={handleFileChange}></input>
+            </div>
         </div>
     );
 }
