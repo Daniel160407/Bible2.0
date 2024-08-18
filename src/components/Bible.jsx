31166047704ffd4306cb304ba03d735d606ef2ec
Daniel160407 <abulashvilidani@hotmail.com>
2024-08-18 18:27:32 +0400

    Added verse selection in bible

--- src/components/Bible.jsx
+++ src/components/Bible.jsx
@@ -8,11 +8,13 @@ const Bible = () => {
     const [versions, setVersions] = useState([]);
     const [books, setBooks] = useState([]);
     const [chapters, setChapters] = useState([]);
+    const [versesAmount, setVersesAmount] = useState([]);
 
     const [selectedVersion, setSelectedVersion] = useState('ახალი გადამუშავებული გამოცემა 2015');
     const [selectedBook, setSelectedBook] = useState('დაბადება');
     const [selectedBookIndex, setSelectedBookIndex] = useState(3);
     const [selectedChapter, setSelectedChapter] = useState(1);
+    const [selectedVerse, setSelectedVerse] = useState(0);
 
     const [verses, setVerses] = useState([]);
 
@@ -37,7 +39,12 @@ const Bible = () => {
                 for(let i = 1; i <= data.tavi[0].cc; i++){
                     chapterArray.push(i);
                 }
+                const verseArray = [];
+                for(let i = 1; i <= data.muxli[0].cc; i++){
+                    verseArray.push(i);
+                }
                 setChapters(chapterArray);
+                setVersesAmount(verseArray);
                 setSelectedChapter(1);
                 setVerses(data.bibleData);
             });
@@ -84,6 +91,24 @@ const Bible = () => {
             });
     }
 
+    const handleVerseChange = (e) => {
+        const verse = e.target.value;
+        setSelectedVerse(verse);
+    
+        for(let i = 0; i < verses.length; i++){
+            if(verses[i].muxli == verse){
+                for(let i = 0; i < verses.length; i++){
+                    document.getElementById(`verse${verses[i].id}`).style.backgroundColor = "#2b3648";
+                }
+    
+                const verseDiv = document.getElementById(`verse${verses[i].id}`);
+                verseDiv.style.backgroundColor = "#4a4a6a";
+    
+                verseDiv.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            }
+        }
+    }    
+
     return (
         <div id='bible'>
             <div className='searchPanel'>
@@ -107,11 +132,16 @@ const Bible = () => {
                         <option value={chapter} key={chapter}>{chapter}</option>
                     ))}
                 </select>
+                <select value={selectedVerse} onChange={handleVerseChange}>
+                    {versesAmount.map(verse => (
+                        <option value={verse} key={verse}>{verse}</option>
+                    ))}
+                </select>
             </div>
             <div id='content'>
                 <h1 id='title'>{selectedBook}</h1>
                 {verses.map(verse => (
-                    <div key={verse.id} className='verse'>
+                    <div id={`verse${verse.id}`} key={verse.id} className='verse'>
                         <h1 className='verse-text'>{verse.bv}</h1>
                         <h1 className='verse-reference'>{selectedBook} {verse.tavi}:{verse.muxli}</h1>
                     </div>
